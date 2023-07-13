69615ea2a81a8d5f43b57ae3fdc6441dcaf2c943
ckw <chia_00@hotmail.com>
2023-07-13 08:03:07 +0800

    change to new firebase

--- dga/firebaseConfig.py
+++ dga/firebaseConfig.py
@@ -6,13 +6,13 @@ load_dotenv()
 
 firebaseConfig = {
     'apiKey': os.getenv('FIREBASE_API_KEY'),
-    'authDomain': "management-transformer.firebaseapp.com",
-    'databaseURL': "https://management-transformer-default-rtdb.asia-southeast1.firebasedatabase.app",
-    'projectId': "management-transformer",
-    'storageBucket': "management-transformer.appspot.com",
-    'messagingSenderId': "26656610083",
-    'appId': "1:26656610083:web:b0f3e1aa42ed6437781e96",
-    'measurementId': "G-PX46R86W7N"
+    'authDomain': "project-dga.firebaseapp.com",
+    'databaseURL': "https://project-dga-default-rtdb.asia-southeast1.firebasedatabase.app",
+    'projectId': "project-dga",
+    'storageBucket': "project-dga.appspot.com",
+    'messagingSenderId': "713322540106",
+    'appId': "1:713322540106:web:10b2ad2045953722364ab3",
+    'measurementId': "G-824K53MBXG"
 }
 
 firebase = pyrebase.initialize_app(firebaseConfig)
